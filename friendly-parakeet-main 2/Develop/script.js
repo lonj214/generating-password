37348fc04f6c5f207f64af73b0254c16c1b97999
London Jensen <londonjensen@Londons-MacBook-Pro.local>
2023-07-04 13:51:39 -0700

    Added constants

--- friendly-parakeet-main 2/Develop/script.js
+++ friendly-parakeet-main 2/Develop/script.js
@@ -1,5 +1,13 @@
 // Assignment code here
 
+const passwordGenerator = document.getElementById("Password");
+const minNumOfChars = 8;
+const maxNumOfChars = 128;
+
+const specialCharacters = ["!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", ">", "=", "?", "@", "[", "]", "^", "_", "`", "{", "}", "~"];
+const numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const lowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
